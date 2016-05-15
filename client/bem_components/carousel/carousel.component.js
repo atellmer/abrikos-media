39548744ffc7ad5068895bb7a166b2c2d921e391
AlexPlex <atellmer@gmail.com>
2016-05-15 18:42:46 +0500

    block-6 finished

---
+++ client/bem_components/carousel/carousel.component.js
@@ -0,0 +1,30 @@
+;(function() {
+	'use strict';
+
+	$('.lk-carousel').slick({
+		infinite: true,
+		speed: 300,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					infinite: true
+				}
+			},
+			{
+				breakpoint: 640,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					infinite: true
+				}
+			}
+		]
+	});
+})();
